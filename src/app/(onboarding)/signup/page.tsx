ec8d98d23939f2f3968c09be298efb71eb930aaa
Headbwoi <headbwoi25@gmail.com>
2023-06-27 22:14:12 +0100

    update login/signup contents

--- src/app/(onboarding)/signup/page.tsx
+++ src/app/(onboarding)/signup/page.tsx
@@ -6,15 +6,7 @@ function SignUp() {
       <header className="mb-10">Sign Up Page</header>
 
       <div className="space-y-5 min-w-40">
-        <button className="grid place-items-center px-5 bg-slate-500 h-10 w-28 rounded-md hover:px-7 hover:w-32 duration-300">
-          Sign Up
-        </button>
-
-        <p>or login</p>
-
-        <button className="grid place-items-center px-5 bg-slate-500 h-10 w-28 rounded-md hover:px-7 hover:w-32 duration-300">
-          Login
-        </button>
+        <p>This is the Signup Page</p>
       </div>
     </div>
   )
